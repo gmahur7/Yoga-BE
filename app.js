4cb5710041c22e589d95cfae2ac8210b97ef2010
gmahur7 <gm7017308@gamil.com>
2024-09-16 12:32:40 +0530

    cors

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const domain=process.env.DOMAIN
 
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
-const { isAuthenticated } = require('./Helpers/JWT_Auth')
+// const { isAuthenticated } = require('./Helpers/JWT_Auth')
 
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['https://app.tandenspine.io','http://localhost:5173'],
     credentials: true, // This allows the server to accept credentials (cookies, headers, etc.)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization']
