2a9c94adabc3895613b1ea05001dba57160da091
gmahur7 <gm7017308@gamil.com>
2024-09-20 12:55:47 +0530

    admin

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const { isAuthenticated } = require('./Helpers/JWT_Auth')
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'],
+    origin: ['https://app.tandenspine.io', 'https://admin.tandenspine.io'],
     credentials: true, // This allows the server to accept credentials (cookies, headers, etc.)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization']
