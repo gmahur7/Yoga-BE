e93030a65421d3ffa5a66121096c90c73f5edb41
gmahur7 <gm7017308@gamil.com>
2024-10-23 12:54:40 +0530

    again

--- app.js
+++ app.js
@@ -59,36 +59,43 @@ app.get('/', (req, resp) => {
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
 // Verify webhook signature
-function verifyWebhookSignature(req, res, next) {
-  const receivedHmac = req.headers['interakt-signature'];
-  const dataString = JSON.stringify(req.body);
+// function verifyWebhookSignature(req, res, next) {
+//   const receivedHmac = req.headers['interakt-signature'];
+//   const dataString = JSON.stringify(req.body);
 
-  function verifyHmac(receivedHmac, dataString, key) {
-    const generatedHmac = CryptoJS.HmacSHA256(dataString, key);
-    const generatedHmacBase64 = "sha256=" + CryptoJS.enc.Hex.stringify(generatedHmac);
-    return generatedHmacBase64 === receivedHmac;
-  }
+//   function verifyHmac(receivedHmac, dataString, key) {
+//     const generatedHmac = CryptoJS.HmacSHA256(dataString, key);
+//     const generatedHmacBase64 = "sha256=" + CryptoJS.enc.Hex.stringify(generatedHmac);
+//     return generatedHmacBase64 === receivedHmac;
+//   }
 
-  if (verifyHmac(receivedHmac, dataString, WEBHOOK_SECRET)) {
-    next();
-  } else {
-    res.status(401).send('Invalid signature');
-  }
-}
+//   if (verifyHmac(receivedHmac, dataString, WEBHOOK_SECRET)) {
+//     next();
+//   } else {
+//     res.status(401).send('Invalid signature');
+//   }
+// }
 
-// Webhook endpoint to receive messages
-app.post('/webhook', verifyWebhookSignature, (req, res) => {
-  const { message,customer } = req.body.data;
-  console.log(customer,message)
+// // Webhook endpoint to receive messages
+// app.post('/webhook', verifyWebhookSignature, (req, res) => {
+//   const { message,customer } = req.body.data;
+//   console.log(customer,message)
 
-  console.log("body: "+req.body)
+//   console.log("body: "+req.body)
 
-  // messages.forEach(message => {
-  //   console.log(`Received message from ${message.from}: ${message.text.body}`);
-  //   handleUserResponse(message);
-  // });
+//   // messages.forEach(message => {
+//   //   console.log(`Received message from ${message.from}: ${message.text.body}`);
+//   //   handleUserResponse(message);
+//   // });
 
-  res.sendStatus(200);
+//   res.sendStatus(200);
+// });
+
+app.post('/webhook', (req, res) => {
+
+  console.log(req.body)
+  console.log(JSON.stringify(req.body,null,2))
+  res.send("done")
 });
 
 //---------------------------USER ROUTES------------------------------------------------
